28f76f50f3ebf7de84d3088ee8b0cf5d2e782bd3
Frontend Oleh Kotlinskyi <oleh.kotlinskyi@abz.agency>
2020-05-24 14:25:34 +0300

    initial server config with eslint

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(3000, () => {
+  console.log('Listen to 3000 port');
+});
+
+module.exports = app;
